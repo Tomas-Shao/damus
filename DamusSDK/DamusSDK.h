31739d4ba4b0f43545d3b00b9d7ad54ccf0053c9
tomas.shao <tomas.shao.hk@outlook.com>
2023-04-18 23:43:31 +0800

    damus sdk

---
+++ DamusSDK/DamusSDK.h
@@ -0,0 +1,18 @@
+//
+//  DamusSDK.h
+//  DamusSDK
+//
+//  Created by Tomas Shao on 2023/4/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DamusSDK.
+FOUNDATION_EXPORT double DamusSDKVersionNumber;
+
+//! Project version string for DamusSDK.
+FOUNDATION_EXPORT const unsigned char DamusSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DamusSDK/PublicHeader.h>
+#import <damus-Bridging-Header.h>
+
